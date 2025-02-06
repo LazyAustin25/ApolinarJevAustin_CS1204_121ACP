4ab635ce79dc8cd56093466e50ce8d50c591da8a
20-06036@g.batstate-u.edu.ph <20-06036@g.batstate-u.edu.ph>
2025-02-06 15:00:17 +0800

    Changes Beybe

--- tupal.py
+++ tupal.py
@@ -1,2 +1,6 @@
 my_tupal= 1, 2, 3
 print(my_tupal)
+x, y, z = my_tupal
+print(x)
+print(y)
+print(z)
--- valo_si_rjay.py
+++ valo_si_rjay.py
@@ -1,6 +1,8 @@
 from collections import Counter
-rol = str(input("Enter the roles: "))
-map = str(input("Enter map: "))
+
+rol = input("Enter the roles: ")
+map = input("Enter map: ")
+
 def count_specific_characters(s):
     D_count = s.count('D')
     S_count = s.count('S')
@@ -8,6 +10,21 @@ def count_specific_characters(s):
     I_count = s.count('I')
     return D_count, S_count, C_count, I_count
 
-string = rol
-D_count, S_count, C_count, I_count = count_specific_characters(string)
-print_counts(D_count, S_count, C_count, I_count)
+D_count, S_count, C_count, I_count = count_specific_characters(rol)
+rs = D_count + S_count + C_count + I_count
+
+if map == "Haven":
+    rc = (I_count // 2)
+elif map == "Ascent":
+    rc = (S_count // 2)
+elif map == "Bind":
+    rc = (C_count // 2)
+else:
+    rc = rs // 5
+
+if ((rs // 5) - rc) <= 0:
+    res = rs
+else:
+    res = rc
+
+print(f"You can have {res} team/s for {map}")
